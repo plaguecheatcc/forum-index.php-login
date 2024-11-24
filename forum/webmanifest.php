9c79408c60c539cf3df1a80ad1479b01da1aa262
plaguecheat.cc <discordcreatore12@proton.me>
2024-11-24 14:12:50 -0800

    Create webmanifest.php

---
+++ forum/webmanifest.php
@@ -0,0 +1,20 @@
+{
+    "name": "Plaguecheat.cc",
+    "short_name": "plague",
+    "description": "Plaguecheat.cc Premium CS:GO HvH and Legit Cheat",
+    "icons": [
+        {
+            "src": "\/forum\/data\/assets\/logo\/unsplash_aJTiW00qqtI.png",
+            "sizes": "192x192",
+            "purpose": "any"
+        }
+    ],
+    "lang": "en-US",
+    "dir": "ltr",
+    "display": "standalone",
+    "scope": "\/forum\/",
+    "id": "\/forum\/?_pwa=1",
+    "start_url": "\/forum\/?_pwa=1",
+    "background_color": "#121212",
+    "theme_color": "#5b86c6"
+}
